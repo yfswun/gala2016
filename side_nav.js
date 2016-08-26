1e2b370d00ad87b70f5441e8a341a4c9fd750772
Sylvia Wun <yfswun@yahoo.com>
2016-08-25 17:08:16 -0700

    initial

---
+++ side_nav.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+	document.getElementById("side_nav").style.width = "200px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("side_nav").style.width = "0";
+}
---
+++ sponsorship_desc.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	var accordion_head = $('.accordion > li > a');
+	var accordion_body = $('.accordion li > .sub-menu');
+
+	// Open the first tab on load
+	accordion_head.first().addClass('active').next().slideDown('normal');
+
+	// Click function
+	accordion_head.on('click', function(event) {
+
+		// Disable header links
+		event.preventDefault();
+
+		// Show and hide the tabs on click
+		if ($(this).attr('class') != 'active') {
+			accordion_body.slideUp('normal');
+			$(this).next().stop(true,true).slideToggle('normal');
+			accordion_head.removeClass('active');
+			$(this).addClass('active');
+		}
+	});
+});
